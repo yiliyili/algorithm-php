66b2f42dac9603dc76ee5e2745397fcd5c867852
yiliyili <1724335344@qq.com>
2020-05-23 18:37:31 +0800

    0-1背包问题

--- src/Other/DynamicProgramming.php
+++ src/Other/DynamicProgramming.php
@@ -9,8 +9,8 @@
  * -------------------------------------------------------------
  * 思路分析：动态规划原理思想，max(opt(i-1,w),wi+opt(i-1,w-wi)) 当中最大值，opt(i-1,w-wi)指上一个最优解
  * -------------------------------------------------------------
- * 一个承受最大重量为W的背包，现在有n个物品，每个物品重量为t, 每个物品的价值为v。
- * 要使得这个背包重量最大(但不能超过W),同时又需要背包的价值最大
+ * 一个承受最大重量/容量为W的背包，现在有n个物品，每个物品重量/容量为t, 每个物品的价值为v。
+ * 要使得这个背包重量/容量最大(但不能超过W),同时又需要背包的价值最大
  */
 
 // +--------------------------------------------------------------------------
@@ -20,45 +20,40 @@
 /**
  * DynamicProgramming
  *
- * @param $maxSize
- * @param $goods
- * @param $cost
+ * @param $W  背包可装载的最大容量/重量
+ * @param $goods  所有物品各自容量/重量组成数组
+ * @param $val  所有物品各自价值组成的数组
  * @return mixed
  */
-function DynamicProgramming($maxSize, $goods, $cost)
+function DynamicProgramming($W, $goods, $val)
 {
-    $container  = [];
-    $goodsCount = count($goods);
+    $dp  = [];
+    $N = count($goods);
 
-    //初始化
-    for ($i = 0; $i <= $maxSize; $i++) {
-        $container[0][$i] = 0;
+    //初始化,没有物品或者一开始空间为0,那么值为0
+    for ($w1 = 0; $w1 <= $W; $w1++) {
+        $dp[0][$w1] = 0;
     }
-    for ($j = 0; $j <= $goodsCount; $j++) {
-        $container[$j][0] = 0;
+    for ($n1 = 0; $n1 <= $N; $n1++) {
+        $dp[$n1][0] = 0;
     }
-    for ($j = 1; $j <= $goodsCount; $j++) {
-        for ($i = 1; $i <= $maxSize; $i++) {
-            $container[$j][$i] = $container[$j - 1][$i];
-            //不大于最大的w=15
-            if ($goods[$j - 1] <= $maxSize) {
-                if (!isset($container[$j - 1][$i - $goods[$j - 1]])) {
-                    continue;
-                }
-                //wi+opt(i-1,wi)
-                $tmp = $container[$j - 1][$i - $goods[$j - 1]] + $cost[$j - 1];
-                //opt(i-1,w),wi+opt(i-1,w-wi) => 进行比较
-                if ($tmp > $container[$j][$i]) {
-                    $container[$j][$i] = $tmp;
-                }
+
+    for ($i = 1; $i <= $N; $i++) {//选择第i个物品
+        for ($j = 1; $j <= $W; $j++) {//容量为j
+            if ($j - $goods[$i-1] < 0) {
+                // 当前背包容量装不下，只能选择不装入背包
+                $dp[$i][$j] = $dp[$i - 1][$j];
+            } else {
+                $dp[$i][$j] = max($dp[$i - 1][$j-$goods[$i-1]] + $val[$i-1], $dp[$i - 1][$j]);
             }
         }
     }
-    return $container[$j - 1][$i - 1];
+    // var_dump($dp);//打印查看最大值在二维数组中位置
+    return $dp[$N][$W];
 }
 
 // +--------------------------------------------------------------------------
 // | 方案测试    | php `this.php` || PHPStorm -> 右键 -> Run `this.php`
 // +--------------------------------------------------------------------------
-
-echo DynamicProgramming(15, array (3, 4, 5, 6), array (8, 7, 4, 9));
+//故意加了一个13,打印数组看存放过程
+echo DynamicProgramming(15, array (3, 4, 5, 6,13), array (8, 7, 4, 9,25));
